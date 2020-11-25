2eb42240d507e0790634311a00258081ac7433b5
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2020-11-25 11:31:52 +0700

    Create draw-notify-to-console.js

---
+++ draw-notify-to-console.js
@@ -0,0 +1,7 @@
+/**
+  * @Credit: https://codelearn.io/
+  */
+function n(n) {
+  return "background:" + n + "; color:white; padding: 5px; font-size: 40px; border-radius: 10px; margin: 1px;"
+}
+console.log("%cF%cP%cT%cCODELEARN.IO", n("#084aa0"), n("#f36f21"), n("#49b644"), n("#ff6d3a"));
