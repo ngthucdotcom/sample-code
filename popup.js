2a796921501aedda65219854386be0fb3bf0f63a
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2022-05-30 14:47:12 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -11,5 +11,3 @@ function openPopup(url, topRatio = 0.25, leftRatio = 0.25) {
     var features = 'height=600,width=800,top='+theTop+',left='+theLeft+',toolbar=0,location=0,directories=0,status=0,menubar=1,scrollbars=1,resizable=1'
     window.open(handleEmptyConfigValue(url, '//example.com'), "_blank", features);
 }
-
-export {openPopup}
